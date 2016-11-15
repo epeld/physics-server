706237490590d811a677f89c451eeec6130c70e6
Erik Peldan <epe@trifork.com>
2016-11-15 20:23:12 +0100

    Get closer to parsing argument lists

--- extract-api.el
+++ extract-api.el
@@ -14,7 +14,7 @@
     (while (string-match regex string next)
       (setq next (match-end 0))
       (push (match-string-no-properties 0 string) result))
-    result))
+    (reverse result)))
 
 
 (defun split (string)
@@ -23,23 +23,46 @@
 
 ;;
 ;;  ODE API parsing
-;; 
+;;
+
+(defvar ode-arg-list-regex "\\s-*\\([a-zA-Z0-9]+\\)\\(\\*\?\\s-\\*\?\\)\\([a-zA-Z]+\\)\\s-*")
+
 (defun cfun-arg (arg)
   "Parse a C-function argument definition"
-  (let ((parts (split arg)))
-    (list :type (car parts)
-          :name (cadr parts))))
+  (string-match ode-arg-list-regex arg)
+  (list :type (if (search "*" (match-string 2 arg))
+                  (list :pointer (match-string 1 arg))
+                (match-string 1 arg)) 
+          :name (match-string 3 arg)))
+
+
+(defun cfun-args (string)
+  (if (string-match "^\\s-*\\([a-zA-Z]+\\)\\s-*$" string)
+      (list :type (match-string 1 string)
+            :name "arg1")
+    
+    (if (or (string-match "^\\s-*void\\s-*$" string)
+            (string-match "^\\s-*$" string))
+        nil
+
+      (let ((m (all-matches ode-arg-list-regex string)))
+        (unless m
+          (error "Strange args list: %s" string))
+        (mapcar #'cfun-arg m)))))
 
 
 (defun ode-cfun (string)
   "Parse a single ODE_API definition"
-  (let (parts args fun)
-    (setq parts (all-matches "[^(]+" string))
-    (setq args (nth 1 parts))
-    (setq fun (nth 0 parts))
-    (list :return-type (nth 1 (split fun))
-          :name (nth 2 (split fun))
-          :args (mapcar #'cfun-arg (all-matches "[^(),;]+" args)))))
+  "ODE_API\\([^(]+\\)(\\(.*\\))"
+  (let (args fun parts)
+    (unless (string-match "ODE_API\\([^(]+\\)(\\([^)]*\\))" string)
+      (error "Not a valid function %s" string))
+    (setq fun (match-string 1 string))
+    (setq args (match-string 2 string))
+    (setq parts (split fun))
+    (list :return-type (nth 0 parts)
+          :name (nth 1 parts)
+          :args (cfun-args args))))
 
 
 (defun list-ode-headers (&optional ode-dir)
@@ -64,7 +87,7 @@
   (let (ode-api headers)
 
     (setq headers (list-ode-headers "/usr/local/include/ode"))
-    (setq ode-api (mapcar #'header-to-ode-api headers))
+    (setq ode-api (mapcar #'header-to-ode-api (subseq headers 0 1)))
   
     (switch-to-buffer-other-window "ode-api")
     (with-current-buffer "ode-api"
