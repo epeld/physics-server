5b2a6c3f062a1f5365a6a59194a3dd7ce0da09a1
Erik Peldan <epe@trifork.com>
2016-11-15 14:52:15 +0100

    Simplify api

--- test.lisp
+++ test.lisp
@@ -8,9 +8,16 @@
    
 (use-foreign-library libode)
 
+(defmethod translate-name-from-foreign ((spec string)
+                                        (package (eql *package*))
+                                        &optional varp)
+  (let ((name (translate-camelcase-name (subseq spec 1)
+                                        :special-words '("ERP" "CFM" "ODE"))))
+    (if varp (intern (format nil "*~a*" name)) name)))
+
 ;;
 ;; Info
-(defcfun ("dGetConfiguration" get-configuration) :string)
+(defcfun "dGetConfiguration" :string)
 
 (defun is-single-precision-p ()
   (search "single_precision" (get-configuration)))
@@ -49,15 +56,15 @@
   (:all #.(lognot 0)))
 
 
-(defcfun ("dInitODE2" init) :uint (flags init-flags))
-(defcfun ("dCloseODE" uninit) :void)
+(defcfun "dInitODE2" :uint (flags init-flags))
+(defcfun "dCloseODE" :void)
 
-(defcfun ("dAllocateODEDataForThread" allocate-data-for-thread) :uint (flags allocate-flags))
+(defcfun "dAllocateODEDataForThread" :uint (flags allocate-flags))
 
 ;; Go ahead and initialize everything directly
 ;; (assumes we are main thread)
-(init '(:none))
-(allocate-data-for-thread '(:all))
+(init-ode-2 '(:none))
+(allocate-ode-data-for-thread '(:all))
 
 ;;
 ;;  World API
@@ -66,41 +73,41 @@
 (defctype space-id :pointer)
 
 ;; Creation
-(defcfun ("dWorldCreate" create-world) world-id)
-(defcfun ("dWorldDestroy" destroy-world) :void (world-id world-id))
+(defcfun "dWorldCreate" world-id)
+(defcfun "dWorldDestroy" :void (world-id world-id))
 
 ;; Gravity
-(defcfun ("dWorldSetGravity" set-gravity) :void
+(defcfun "dWorldSetGravity" :void
   (world-id world-id)
   (x ode-real)
   (y ode-real)
   (z ode-real))
 
-(defcfun ("dWorldGetGravity" get-gravity) :void 
+(defcfun "dWorldGetGravity" :void 
   (world-id world-id)
   (vector (:pointer (:struct vector3))))
 
 ;; ERP
-(defcfun ("dWorldSetERP" set-erp) :void (world-id world-id) (erp ode-real))
-(defcfun ("dWorldGetERP" get-erp) ode-real (world-id world-id))
+(defcfun "dWorldSetERP" :void (world-id world-id) (erp ode-real))
+(defcfun "dWorldGetERP" ode-real (world-id world-id))
 
 ;; CFM
-(defcfun ("dWorldSetCFM" set-cfm) :void (world-id world-id) (cfm ode-real))
-(defcfun ("dWorldGetCFM" get-cfm) ode-real (world-id world-id))
+(defcfun "dWorldSetCFM" :void (world-id world-id) (cfm ode-real))
+(defcfun "dWorldGetCFM" ode-real (world-id world-id))
 
 ;; Update / Stepping
-(defcfun ("dWorldStep" step-world) :void (world-id world-id) (step-size ode-real))
-(defcfun ("dWorldQuickStep" quick-step-world) :void (world-id world-id) (step-size ode-real))
+(defcfun "dWorldStep" :void (world-id world-id) (step-size ode-real))
+(defcfun "dWorldQuickStep" :void (world-id world-id) (step-size ode-real))
 
 ;;
 ;; Body
 ;; 
 (defctype body-id :pointer)
 
-(defcfun ("dBodyCreate" create-body) body-id
+(defcfun "dBodyCreate" body-id
   (world-id world-id))
 
-(defcfun ("dBodyDestroy" destroy-body) body-id
+(defcfun "dBodyDestroy" body-id
   (body-id body-id))
 
 ;;
@@ -109,52 +116,52 @@
 (defctype geom-id :pointer)
 
 ;; Creation
-(defcfun ("dCreateBox" create-box) geom-id
+(defcfun "dCreateBox" geom-id
   (space-id space-id)
   (x ode-real)
   (y ode-real)
   (z ode-real))
 
-(defcfun ("dCreateSphere" create-sphere) geom-id
+(defcfun "dCreateSphere" geom-id
   (space-id space-id)
   (radius ode-real))
 
-(defcfun ("dCreatePlane" create-plane) geom-id
+(defcfun "dCreatePlane" geom-id
   (space-id space-id)
   (a ode-real)
   (b ode-real)
   (c ode-real)
   (d ode-real))
 
-(defcfun ("dGeomDestroy" destroy-geom) :void
+(defcfun "dGeomDestroy" :void
   (geom-id geom-id))
 
 
 ;; Bodies
-(defcfun ("dGeomGetBody" get-geom-body) body-id
+(defcfun "dGeomGetBody" body-id
   (geom-id geom-id))
 
-(defcfun ("dGeomSetBody" set-geom-body) :void
+(defcfun "dGeomSetBody" :void
   (geom-id geom-id)
   (body-id body-id))
 
 ;; Position
-(defcfun ("dGeomSetPosition" set-geom-position) :void
+(defcfun "dGeomSetPosition" :void
   (geom-id geom-id)
   (x ode-real)
   (y ode-real)
   (z ode-real))
 
-(defcfun ("dGeomGetPosition" get-geom-position) (:pointer ode-real)
+(defcfun "dGeomGetPosition" (:pointer ode-real)
   (geom-id geom-id))
 
 
 ;; Rotation
-(defcfun ("dGeomGetRotation" get-geom-rotation) (:pointer (:struct matrix3))
+(defcfun "dGeomGetRotation" (:pointer (:struct matrix3))
   (geom-id geom-id))
 
 
-(defcfun ("dGeomSetRotation" set-geom-rotation) :void
+(defcfun "dGeomSetRotation" :void
   (geom-id geom-id)
   (r (:pointer (:struct matrix3))))
 
@@ -162,26 +169,26 @@
 ;; Space
 ;;
 
-(defcfun ("dHashSpaceCreate" create-hash-space) space-id (space-id space-id))
+(defcfun "dHashSpaceCreate" space-id (space-id space-id))
 
-(defcfun ("dSpaceAdd" add-geom-to-space) :void (space-id space-id) (geom-id geom-id))
-(defcfun ("dSpaceRemove" remove-geom-from-space) :void (space-id space-id) (geom-id geom-id))
-(defcfun ("dSpaceClean" clear-space) :void (space-id space-id))
+(defcfun "dSpaceAdd" :void (space-id space-id) (geom-id geom-id))
+(defcfun "dSpaceRemove" :void (space-id space-id) (geom-id geom-id))
+(defcfun "dSpaceClean" :void (space-id space-id))
 
-(defcfun ("dSpaceGetNumGeoms" get-num-geoms) :int (space-id space-id))
-(defcfun ("dSpaceGetGeom" get-geom) geom-id (space-id space-id) (index :int))
+(defcfun "dSpaceGetNumGeoms" :int (space-id space-id))
+(defcfun "dSpaceGetGeom" geom-id (space-id space-id) (index :int))
 
 ;;
 ;; Collision
 ;;
 
-(defcfun ("dSpaceCollide" space-collide) :void
+(defcfun "dSpaceCollide" :void
   (space-id space-id)
   (data :pointer)
   (near-callback :pointer))
 
 ;; TODO figure out contact geom format
-(defcfun ("dCollide" collide) :int
+(defcfun "dCollide" :int
   (o1 geom-id)
   (o2 geom-id)
   (flags :int)
@@ -192,14 +199,14 @@
 ;;  Rotation
 ;;
 
-(defcfun ("dRFromAxisAndAngle" rotation-around-axis) :void
+(defcfun "dRFromAxisAndAngle" :void
   (r (:pointer (:struct matrix3)))
   (ax ode-real)
   (ay ode-real)
   (bz ode-real)
   (angle ode-real))
 
-(defcfun ("dRSetIdentity" set-identity) :void
+(defcfun "dRSetIdentity" :void
   (r (:pointer (:struct matrix3))))
 
 ;;
@@ -237,7 +244,7 @@
   (fdir (:struct vector3)))
 
 
-(defcfun ("dJointCreateContact" create-contact) joint-id
+(defcfun "dJointCreateContact" joint-id
   (world-id world-id)
   (joint-group-id joint-group-id)
   (contact (:pointer (:struct contact))))
@@ -245,19 +252,19 @@
 ;;
 ;;  Joint Groups
 ;; 
-(defcfun ("dJointGroupCreate" create-joint-group) joint-group-id
+(defcfun "dJointGroupCreate" joint-group-id
   (max-size :int))
 
-(defcfun ("dJointGroupDestroy" destroy-joint-group) :void
+(defcfun "dJointGroupDestroy" :void
   (joint-group-id joint-group-id))
 
 
 ;; Clear
-(defcfun ("dJointGroupEmpty" clear-joint-group) :void
+(defcfun "dJointGroupEmpty" :void
   (joint-group-id joint-group-id))
 
 
-(defcfun ("dJointAttach" attach-joint) :void
+(defcfun "dJointAttach" :void
   (joint-id joint-id)
   (body-id-1 body-id)
   (body-id-2 body-id))
